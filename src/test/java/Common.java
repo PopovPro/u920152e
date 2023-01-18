0611d52bf629d98b3fa34694e7ce128fa578532a
Igor <igoryan90@gmail.com>
2023-01-18 22:18:00 +0400

    structure and config add

--- src/test/java/Common.java
+++ src/test/java/Common.java
@@ -1,3 +1,18 @@
+import io.restassured.RestAssured;
+
+import java.io.IOException;
+import java.util.Properties;
+
 public class Common {
 
+    public Common() {
+        Properties prop = new Properties();
+        try {
+            prop.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
+
+            RestAssured.baseURI = prop.getProperty("BASE_URL");
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
 }
